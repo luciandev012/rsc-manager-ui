e96345261e799e55a530cd72ebc080d4b3a5f197
thang1224 <thanguk04@gmail.com>
2022-07-21 04:19:04 -0700

    delete order

--- src/apis/order.js
+++ src/apis/order.js
@@ -2,6 +2,6 @@ import API from "../helper/axios";
 
 export const getAllOrder = () => API.get("/Order/GetAllOrder");
 
-export const deleteOrder = (id) => API.delete(`/Order/DeleteOrder?id=${id}`);
+export const deleteOrder = (id) => API.delete(`/Order/DeleteOder?id=${id}`);
 
 export const updateOrder = (order) => API.post("/Order/UpdateOrder", order);
